b78bcddfdfd90860434de304fdc45eb0738614da
Phil <PhilboBaggins@users.noreply.github.com>
2023-02-19 22:48:01 +1100

    Fix file names

---
+++ ProductionFiles-v1.1/fix-names.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd "$(dirname -- "$0")"
+
+mv -iv "LEDs-LED_-_5mm_-_Rough.stl"       "LED - 5mm - Rough.stl"
+mv -iv "LEDs-LED_-_5mm_-_Rough_-_5x.stl"  "LED - 5mm - Rough - 5x.stl"
+mv -iv "LEDs-LED_-_5mm_-_Smooth.stl"      "LED - 5mm - Smooth.stl"
+mv -iv "LEDs-LED_-_5mm_-_Smooth_-_5x.stl" "LED - 5mm - Smooth - 5x.stl"
